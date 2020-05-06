9522287f35f6b1a505dbfc156d256d9fe64e14cf
Jonyelson Lopes <jonyelson20@gmail.com>
2020-05-05 23:01:12 -0300

    Finalizado o Planner

--- semana4/planner/script.js
+++ semana4/planner/script.js
@@ -1,9 +1,36 @@
 function adicionarTarefa() {
+    let tarefa = document.getElementById("tarefa").value;
     let diaSemana = document.querySelector("select").value;
-    console.log(diaSemana);
-
-
-    document.querySelector("div").innerHTML("teco");
-
+    switch (diaSemana) {
+        case '1':
+            document.getElementById("segunda").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+        case '2':
+            document.getElementById("terca").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+        case '3':
+            document.getElementById("quarta").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+        case '4':
+            document.getElementById("quinta").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+        case '5':
+            document.getElementById("sexta").innerHTML += "<p>" + tarefa + "</p>";
+            console.log("deu certo");
+            document.getElementById("tarefa").value = "";
+            break;
+        case '6':
+            document.getElementById("sabado").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+        case '7':
+            document.getElementById("domingo").innerHTML += "<p>" + tarefa + "</p>";
+            document.getElementById("tarefa").value = "";
+            break;
+    }
 
 }
