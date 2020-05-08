bff421bcb55e39f8862bacf0850b14d162778de3
Jonyelson Lopes <jonyelson20@gmail.com>
2020-05-08 16:52:49 -0300

    Resoluções dos exercicios js

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,40 @@
+//indica as questões
+/* indica as respostas das questões */
+
+// Exercícios de interpretação de código
+
+// 1
+/*
+    A função é um conversor de moeda de dolar para real que recebe um
+parametro em sua invoção no caso o valor em dolar foi 100, quando executada
+solicita aõ usuario que coloque o valor da cotação do dolar neste exemplo vamos
+considera com 4 então  em seu processamento a constante 'meuDinheiro' multiplica
+o valor em dolar pela cotação e retorna a saida em real no console que seria 'R$ 400'
+*/
+
+// 2 
+/* esta função serve para calcular investimentos com base no tipo e no valor,
+em seu escopo ela possui uma estrutura condicional que verifica o tipo de investimento
+e apartir dai faz o calculo do valor investido. para o novoMontante que têm o tipo Ações
+e valor 150 ela vai retorna a quantia de 165. ja para o segundoMontante que possui
+tipo Tesouro direto e valor 200 ela vai verifica que o tipo não exiti e vai cair no
+default, logo vai avisar o usuario que o tipo informado é incorreto.
+*/
+
+//3
+/*
+    O código faz a declaração de 3 arrays sendo dois arrays vazios e um com varios numeros,
+    depois em uma laço ele pecorre todo o array de numeros e verifica se o numero do indice
+    atual dividido por 2 é = 0 se for ele armazena em um dos array vazio, caso contrario
+    ele armazena no outro array vazio, ao fim do laço temos todos os array preenchidos.
+    depois no cosole o código mostrar a quantidade total de numeros em cada array.
+*/
+
+//4
+/*
+
+*/
+
+
+
+
