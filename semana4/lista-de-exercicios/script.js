6b594900168f6f4e8aea5b67d6a028b53288ec38
Jonyelson Lopes <jonyelson20@gmail.com>
2020-05-09 13:54:13 -0300

    Resovildo os Exercícios de interpretação de código

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,5 +1,5 @@
-//indica as questões
-/* indica as respostas das questões */
+//indica os Exercícios
+/* indica as respostas dos exercícios */
 
 // Exercícios de interpretação de código
 
@@ -13,7 +13,8 @@ o valor em dolar pela cotação e retorna a saida em real no console que seria '
 */
 
 // 2 
-/* esta função serve para calcular investimentos com base no tipo e no valor,
+/*
+    Esta função serve para calcular investimentos com base no tipo e no valor,
 em seu escopo ela possui uma estrutura condicional que verifica o tipo de investimento
 e apartir dai faz o calculo do valor investido. para o novoMontante que têm o tipo Ações
 e valor 150 ela vai retorna a quantia de 165. ja para o segundoMontante que possui
@@ -23,7 +24,7 @@ default, logo vai avisar o usuario que o tipo informado é incorreto.
 
 //3
 /*
-    O código faz a declaração de 3 arrays sendo dois arrays vazios e um com varios numeros,
+        O código faz a declaração de 3 arrays sendo dois arrays vazios e um com varios numeros,
     depois em uma laço ele pecorre todo o array de numeros e verifica se o numero do indice
     atual dividido por 2 é = 0 se for ele armazena em um dos array vazio, caso contrario
     ele armazena no outro array vazio, ao fim do laço temos todos os array preenchidos.
@@ -32,8 +33,12 @@ default, logo vai avisar o usuario que o tipo informado é incorreto.
 
 //4
 /*
-
+    O código verifica qual o maior e menor numero dentro do array, o código estaria mais
+    legivel se as variaveis numero1 e numero2 estivessem representadas respectivamente
+    por menorNumero e maiorNumero. Aõ final ele imprime no console o menor e maior
+    numero, no caso menor = -10 e maior = 1590.
 */
+
 
 
 
