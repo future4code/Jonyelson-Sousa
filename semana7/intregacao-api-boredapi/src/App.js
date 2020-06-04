644c70aac4a87f1f4a3ead3066cc974688b5aaff
Jonyelson Lopes <jonyelson20@gmail.com>
2020-06-04 16:36:38 -0300

    criado a intregracao com a api boredApi

---
+++ semana7/intregacao-api-boredapi/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Atividades aleatorias</h1>
+      <p>relaxe esse tédio</p>
+      <button>Gerar atividade</button>
+    </div>
+  );
+}
+
+export default App;
