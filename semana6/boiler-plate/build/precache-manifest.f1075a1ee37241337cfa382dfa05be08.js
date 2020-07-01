a3ade8ed1967846d29f92ebdd95a38a198544d34
Jonyelson Lopes <jonyelson20@gmail.com>
2020-07-01 14:12:50 -0300

    arquivos antigos

---
+++ semana6/boiler-plate/build/precache-manifest.f1075a1ee37241337cfa382dfa05be08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f548e8f7d029bf1d89bd2d3f8cf493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d9cd2a7664deacb74ac",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "5d9cd2a7664deacb74ac",
+    "url": "/static/js/main.9464dcbb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
