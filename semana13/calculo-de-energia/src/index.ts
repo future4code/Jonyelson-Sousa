a71ce66fe739470979aaad448035359629b14a8f
Jonyelson Lopes <jonyelson20@gmail.com>
2020-08-05 16:57:23 -0300

    Resolvido exercício 2

--- semana13/calculo-de-energia/src/index.ts
+++ semana13/calculo-de-energia/src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from "./Client";
+import { Place } from "./Place";
 
 // Exercício 1
 const client: Client = {
@@ -17,3 +18,11 @@ console.log(client.registrationNumber);
 console.log(client.consumedEnergy);
 
 // A) Todos foram impressos, pois se trata de um objeto de interface e logo todas as propriedades são publicas.
+
+// Exercício 2
+
+//const newPlace: Place = new Place();
+
+// A) Não é possível criar uma instância de uma classe abstrata. error TS2511
+
+// B) Poderiamos remover o abstract da classe Place.
